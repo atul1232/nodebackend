4f1b0000d31bf52fca2b88d1d6b6e5b6cf848bef
chirag <chirag@capanicus.com>
2024-10-01 18:54:34 +0530

    update

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 # This Is backend Cource in Node.js 
+# Course Source : https://github.com/hiteshchoudhary/chai-backend/blob/main/src/index.js
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-const app = express();
 
 connectDB()
     .then(() => {
@@ -15,5 +14,5 @@ connectDB()
         });
     })
     .catch((error) => {
-        console.log("Error: MongoDB connection failed",error);
+        console.log("Error: MongoDB connection failed", error);
     });
